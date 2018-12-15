a544d4d7082e2dbb3eea53f2e489243455021c67
3a532049 <shelly213334@gmail.com>
2018-12-15 22:49:20 +0800

    定義TaskController的store方法，使其可存放新增的task，讓 name 欄位為必填，且它必須少於 255 字元

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -18,8 +18,29 @@ class TaskController extends Controller
         $this->middleware('auth'); //限制任務路由只讓已認證的使用者存取(中介層),在控制器的建構子中增加 middleware 方法的呼叫
     }
 
+    /**
+     * 顯示使用者所有任務的清單。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
     public function index(Request $request) //index 方法回傳此視圖
     {
         return view('tasks.index');
+    }
+
+    /**
+     * 建立新的任務。
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this -> validate ($request, [
+            'name' => 'required|max:255', //讓 name 欄位為必填，且它必須少於 255 字元
+        ]);
+
+        // Create The Task...
     }
 }
