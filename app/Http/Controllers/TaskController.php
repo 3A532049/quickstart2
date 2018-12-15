47ccfcb0a0807e298e45a443c5864325e5e8bd6a
3a532049 <shelly213334@gmail.com>
2018-12-15 22:28:51 +0800

    認證TaskController，即可認證所有的任務路由

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class TaskController extends Controller
 {
-    //
+    /**
+     * 建立一個新的控制器實例。
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth'); //限制任務路由只讓已認證的使用者存取(中介層),在控制器的建構子中增加 middleware 方法的呼叫
+    }
 }
